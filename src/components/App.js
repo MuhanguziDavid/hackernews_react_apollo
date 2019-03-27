8eaf5f7e12b9d6c9094cab3ef950a337448c2096
David Muhanguzi <muhunksdavie95@gmail.com>
2019-03-27 16:35:34 +0300

    - add graphQl mutation - add routing - add a header section

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
 import LinkList from './LinkList'
+import CreateLink from './CreateLink';
+import Header from './Header';
 
 class App extends Component {
   render() {
-    return <LinkList />
+    return (
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+          </Switch>
+        </div>
+      </div>
+    )
   }
 }
 
