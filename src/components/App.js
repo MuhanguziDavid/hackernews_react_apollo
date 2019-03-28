c9bb657d9a4fab376cad39d1e47c779642aff6a1
David Muhanguzi <muhunksdavie95@gmail.com>
2019-03-28 13:03:06 +0300

    - add authentication

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 import LinkList from './LinkList'
 import CreateLink from './CreateLink';
 import Header from './Header';
+import Login from './Login';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={LinkList} />
             <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </div>
       </div>
